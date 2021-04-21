50f4ffb7f2eecf1fa7622f246dd61fa790862e08
Xuanye Wong <xuanye.wan@gmail.com>
2021-04-21 10:02:50 +0800

    兼容2.3版本 回调参数变更的问题

--- src/DotXxlJob.Core/Model/HandleCallbackParam.cs
+++ src/DotXxlJob.Core/Model/HandleCallbackParam.cs
@@ -23,7 +23,36 @@ namespace DotXxlJob.Core.Model
         public long LogId { get; set; }
         [DataMember(Name = "logDateTim",Order = 2)]
         public long LogDateTime { get; set; }
+
+
+        /// <summary>
+        /// 2.3.0以前版本
+        /// </summary>
         [DataMember(Name = "executeResult",Order = 3)]
         public ReturnT ExecuteResult { get; set; }
+
+        /// <summary>
+        /// 2.3.0版本使用的参数
+        /// </summary>
+        [DataMember(Name = "handleCode", Order = 4)]
+        public int HandleCode { 
+            get {
+                if(this.ExecuteResult != null)
+                {
+                    return this.ExecuteResult.Code;
+                }
+                return 500;
+            } 
+        }
+
+        /// <summary>
+        /// 2.3.0版本使用的参数
+        /// </summary>
+        [DataMember(Name = "handleMsg", Order = 5)]
+        public string HandleMsg {
+            get {
+                return this.ExecuteResult?.Msg;
+            }
+        }
     }
 }
