a57721cf908161ec92854593abc0724e9f5ccb36
guochen2 <524014724@qq.com>
2022-01-12 13:31:49 +0800

    1.增加CancellationToken用于任务取消 2.修复在多任务执行情况下因取消或覆盖任务导致的调用bug

--- src/DotXxlJob.Core/TaskExecutors/BeanTaskExecutor.cs
+++ src/DotXxlJob.Core/TaskExecutors/BeanTaskExecutor.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using DotXxlJob.Core.Model;
 
@@ -6,28 +7,28 @@ namespace DotXxlJob.Core.TaskExecutors
     /// <summary>
     /// 实现 IJobHandler的执行器
     /// </summary>
-    public class BeanTaskExecutor:ITaskExecutor
+    public class BeanTaskExecutor : ITaskExecutor
     {
         private readonly IJobHandlerFactory _handlerFactory;
         private readonly IJobLogger _jobLogger;
 
-        public BeanTaskExecutor(IJobHandlerFactory handlerFactory,IJobLogger jobLogger)
+        public BeanTaskExecutor(IJobHandlerFactory handlerFactory, IJobLogger jobLogger)
         {
             this._handlerFactory = handlerFactory;
             this._jobLogger = jobLogger;
         }
-        
+
         public string GlueType { get; } = Constants.GlueType.BEAN;
-        
-        public Task<ReturnT> Execute(TriggerParam triggerParam)
+
+        public Task<ReturnT> Execute(TriggerParam triggerParam, CancellationToken cancellationToken)
         {
             var handler = _handlerFactory.GetJobHandler(triggerParam.ExecutorHandler);
 
             if (handler == null)
             {
-               return Task.FromResult(ReturnT.Failed($"job handler [{triggerParam.ExecutorHandler} not found."));
+                return Task.FromResult(ReturnT.Failed($"job handler [{triggerParam.ExecutorHandler} not found."));
             }
-            var context = new JobExecuteContext(this._jobLogger, triggerParam.ExecutorParams);
+            var context = new JobExecuteContext(this._jobLogger, triggerParam.ExecutorParams, cancellationToken);
             return handler.Execute(context);
         }
     }
