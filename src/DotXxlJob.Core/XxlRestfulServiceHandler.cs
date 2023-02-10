fbe3e1a5ccb68b62d955d4af00dc4375eea728ba
徐超越 <993921@qq.com>
2023-02-10 17:20:06 +0800

    Update XxlRestfulServiceHandler.cs
    bodyText（没有任务参数）为空时会摄氏

--- src/DotXxlJob.Core/XxlRestfulServiceHandler.cs
+++ src/DotXxlJob.Core/XxlRestfulServiceHandler.cs
@@ -106,7 +106,7 @@ namespace DotXxlJob.Core
             {
                 bodyText = await reader.ReadToEndAsync();
             }
-            return bodyText;
+            return bodyText??string.Emppty;
         }
 
         #region rpc service
